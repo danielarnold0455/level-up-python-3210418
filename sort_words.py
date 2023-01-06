e1188113d52c7ec7b3e505e1a9638f6dec28e3f3
Daniel Arnold <daniel.arnold@ansys.com>
2023-01-06 13:58:43 +0100

    Challenge 03
    Sort a string by sorted method

--- sort_words.py
+++ sort_words.py
@@ -3,6 +3,9 @@ def sort_words(words):
     sorted_words.sort(key=str.casefold)
     return ' '.join(sorted_words)
 
+def sorted_words(words):
+    return ' '.join(sorted(words.split(), key=str.casefold))
 
 if __name__ == '__main__':
     print(sort_words('Orange banana Apple apple'))
+    print(sorted_words('Orange banana Apple apple'))
